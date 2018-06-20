1dac83bb707d55c2d51fa2bd34889dfb930e6501
Rafael Cabrera <rafacabrerars@gmail.com>
2018-06-19 23:42:06 -0300

    adicionando <br>

--- teams.php
+++ teams.php
@@ -4,7 +4,7 @@ $data = file_get_contents("http://worldcup.sfg.io/teams");
 $data = json_decode($data,true);
 echo "<h1>Seleções</h1>";
 foreach ($data as $team){
-	echo "{$team['country']} - {$team['fifa_code']} - GRUPO {$team['group_letter']}\n";
+	echo "{$team['country']} - {$team['fifa_code']} - GRUPO {$team['group_letter']}\n<br>";
 
 }
 
